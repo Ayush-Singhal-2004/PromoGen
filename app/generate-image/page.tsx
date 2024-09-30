f8da4c2a499e844269eff0bc07af630d4137d75a
Ayush-Singhal-2004 <ayushsinghal1904@gmail.com>
2024-09-30 13:52:53 +0530

    home page

--- app/generate-image/page.tsx
+++ app/generate-image/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import Link from "next/link"
+import { useTheme } from "next-themes"
+
 export default function GenerateImagePage() {
+
+    const { theme, setTheme } = useTheme();
+
     return (
-        <h1>Generate Image page!!</h1>
+        <div>
+            <nav className="py-5 px-8 flex items-center justify-between border-b border-b-gray-300 dark:border-0">
+                    <h1 className="text-3xl dark:text-white">PromoGen</h1>
+                    <div className="flex items-center gap-8 pr-10">
+                        <Link href={"/"} className="text-white">Home</Link>
+                        <Link href={"/generate-image"} className="text-white">Image</Link>
+                        <Link href={"/generate-video"} className="text-white">Video</Link>
+                    </div>
+            </nav>
+        </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,57 +7,71 @@ import Link from "next/link";
 
 export default function Home() {
 
+    const moveDown = () => {
+        document.getElementById("section-2")?.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+
     return (
         <div className="dark">
-        <AuroraBackground>
-            <motion.div
-            initial={{ opacity: 0.0, y: 40 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-                delay: 0.3,
-                duration: 0.8,
-                ease: "easeInOut",
-            }}
-            >
+            <AuroraBackground>
+                <motion.div
+                initial={{ opacity: 0.0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                    delay: 0.3,
+                    duration: 0.8,
+                    ease: "easeInOut",
+                }}
+                >
 
-                <nav className="py-3 px-8 flex items-center justify-between border-b border-b-gray-300 dark:border-0">
-                    <h1 className="text-3xl dark:text-white">PromoGen</h1>
-                    <div className="flex items-center gap-8 pr-10">
-                        <Link href={"/"} className="text-white">Home</Link>
-                        <Link href={"/generate-image"} className="text-white">Image</Link>
-                        <Link href={"/generate-video"} className="text-white">Video</Link>
-                    </div>
-                </nav>
-                <div className="flex flex-col pt-[10vh] px-[4vw] gap-[5vh]">
-                    <div className="py-5">
-                        <h1 className="text-7xl font-semibold mb-2 dark:to-zinc-300 dark:bg-clip-text text-transparent] dark:text-white">
-                            Unleash Creativity <br/>
-                            with GenAI
-                        </h1>
-                        <p className="text-gray-700 dark:bg-gradient-to-tr dark:from-gray-50 dark:to-gray-300 dark:bg-clip-text dark:text-transparent tracking-wider">
-                            Instant, dynamic banners and videos for every promotion.
-                        </p>
-                    </div>
-                    <div className="h-[35vh] w-[85vw] flex pt-5 gap-8">
-                        <Link href={"/generate-image"}>
-                            <Button variant="default">
+                    <nav className="py-3 px-8 flex items-center justify-between border-b border-b-gray-300 dark:border-0">
+                        <h1 className="text-3xl dark:text-white">PromoGen</h1>
+                        <div className="flex items-center gap-8 pr-10">
+                            <Link href={"/"} className="text-white">Home</Link>
+                            <Link href={"/generate-image"} className="text-white">Image</Link>
+                            <Link href={"/generate-video"} className="text-white">Video</Link>
+                        </div>
+                    </nav>
+                    <div className="flex flex-col pt-[10vh] px-[4vw] gap-[5vh]">
+                        <div className="py-5">
+                            <h1 className="text-7xl font-semibold mb-2 dark:to-zinc-300 dark:bg-clip-text text-transparent] dark:text-white">
+                                Unleash Creativity <br/>
+                                with GenAI
+                            </h1>
+                            <p className="text-gray-700 dark:bg-gradient-to-tr dark:from-gray-50 dark:to-gray-300 dark:bg-clip-text dark:text-transparent tracking-wider">
+                                Instant, dynamic banners and videos for every promotion.
+                            </p>
+                        </div>
+                        <div className="h-[35vh] w-[85vw] flex pt-5 gap-8">
+                            {/* <Link href={"/generate-image"}>
+                                <Button variant="default">
+                                    <p className="text-lg p-4">
+                                        Generate Image
+                                    </p>
+                                </Button>
+                            </Link> */}
+                            <Button variant="default" onClick={moveDown}>
                                 <p className="text-lg p-4">
                                     Generate Image
                                 </p>
                             </Button>
-                        </Link>
-                        <Link href={"/generate-video"}>
-                            <Button variant="default">
-                                <p className="text-lg p-4">
-                                    Generate Video
-                                </p>
-                            </Button>
-                        </Link>
+                            <Link href={"/generate-video"}>
+                                <Button variant="default">
+                                    <p className="text-lg p-4">
+                                        Generate Video
+                                    </p>
+                                </Button>
+                            </Link>
+                        </div>
                     </div>
-                </div>
 
-            </motion.div>
-        </AuroraBackground>
+                </motion.div>
+            </AuroraBackground>
+            <div className="h-screen bg-gradient-to-b from-[#18181B] to-slate-900" id="section-2">
+                
+            </div>
         </div>
     )
 }
